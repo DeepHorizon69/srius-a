93cc7c43fb1c9a2ef7d5f8b4542722534c3551fb
DeepHorizon69 <pengentauofisial@gmail.com>
2026-05-24 16:26:47 +0700

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   compressHTML: true,
 
-  site: 'https://DeepHorizon69.github.io',
-  base: '/srius-a',
+  site: 'https://itsalsa.web.id',
 });
